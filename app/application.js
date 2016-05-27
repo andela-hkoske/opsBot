4bb0adc47bfdd7b51e71ae9f094e89b26621f9d6
Brian Koech <brnkirwa@gmail.com>
2016-05-27 19:36:42 +0300

    Add react, react-router

---
+++ app/application.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import App from './components/main';
+
+render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App} />
+  </Router>), document.getElementById('view'));
